dfda15f4869eda03a41eae66cb3e937cd38b7959
Mark <mortalimmortalko@mail.ru>
2021-05-15 02:17:43 +0300

    upload2

--- models/Reservation.php
+++ models/Reservation.php
@@ -2,8 +2,8 @@
 
 namespace app\models;
 
-use Cassandra\Date;
 use Yii;
+use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "reservation".
@@ -18,48 +18,49 @@ use Yii;
  * @property string|null $mail
  * @property string|null $Number
  * @property int|null $NumberOfPeople
+ * @property string|null $DateAdded
  *
  * @property Cities $idCity0
  * @property Countries $idCountry0
  * @property Employees $idEmployee0
  * @property Resorts $idResort0
  */
-class Reservation extends \yii\db\ActiveRecord
+class Reservation extends ActiveRecord
 {
     /**
      * {@inheritdoc}
      */
-    public static function tableName()
+    public static function tableName(): string
     {
         return 'reservation';
     }
 
-
     /**
      * {@inheritdoc}
      */
-    public function rules()
+    public function rules(): array
     {
 
         return [
             [['idCity', 'idCountry', 'idResort',], 'required', 'message' => "Заполните \"" . $this->getAttributeLabel('{attribute}') . "\""],
             [['idCity', 'idCountry', 'idResort', 'idEmployee', 'LengthOfNights', 'NumberOfPeople'], 'integer', 'message' => 'Только целое число'],
-            [['DateBirth'], 'safe'],
+            [['DateBirth', 'DateAdded'], 'safe'],
             [['mail', 'Number'], 'string', 'max' => 145],
             ['mail', 'email'],
             ['NumberOfPeople', 'number', 'max' => 8, 'min' => 1, 'tooBig' => 'Не более 8', 'tooSmall' => 'Не менее 1'],
             ['LengthOfNights', 'number', 'max' => 16, 'min' => 2, 'tooBig' => 'Не более 16', 'tooSmall' => 'Не менее 2'],
-            [['idCity'], 'exist', 'skipOnError' => true, 'targetClass' => Cities::className(), 'targetAttribute' => ['idCity' => 'idCity']],
-            [['idCountry'], 'exist', 'skipOnError' => true, 'targetClass' => Countries::className(), 'targetAttribute' => ['idCountry' => 'idCountry']],
-            [['idEmployee'], 'exist', 'skipOnError' => true, 'targetClass' => Employees::className(), 'targetAttribute' => ['idEmployee' => 'idEmployee']],
-            [['idResort'], 'exist', 'skipOnError' => true, 'targetClass' => Resorts::className(), 'targetAttribute' => ['idResort' => 'idResorts']],
+            [['idCity'], 'exist', 'skipOnError' => true, 'targetClass' => Cities::class, 'targetAttribute' => ['idCity' => 'idCity']],
+            [['idCountry'], 'exist', 'skipOnError' => true, 'targetClass' => Countries::class, 'targetAttribute' => ['idCountry' => 'idCountry']],
+            [['idEmployee'], 'exist', 'skipOnError' => true, 'targetClass' => Employees::class, 'targetAttribute' => ['idEmployee' => 'idEmployee']],
+            [['idResort'], 'exist', 'skipOnError' => true, 'targetClass' => Resorts::class, 'targetAttribute' => ['idResort' => 'idResorts']],
         ];
     }
 
     /**
      * {@inheritdoc}
      */
-    public function attributeLabels()
+
+    public function attributeLabels(): array
     {
         return [
             'idReservation' => 'Id Reservation',
@@ -72,6 +73,7 @@ class Reservation extends \yii\db\ActiveRecord
             'mail' => 'Почта',
             'Number' => 'Номер',
             'NumberOfPeople' => 'Туристы',
+            'DateAdded' => 'Date Added',
         ];
     }
 
@@ -80,9 +82,9 @@ class Reservation extends \yii\db\ActiveRecord
      *
      * @return \yii\db\ActiveQuery
      */
-    public function getIdCity0()
+    public function getIdCity0(): \yii\db\ActiveQuery
     {
-        return $this->hasOne(Cities::className(), ['idCity' => 'idCity']);
+        return $this->hasOne(Cities::class, ['idCity' => 'idCity']);
     }
 
     /**
@@ -90,7 +92,7 @@ class Reservation extends \yii\db\ActiveRecord
      *
      * @return \yii\db\ActiveQuery
      */
-    public function getIdCountry0()
+    public function getIdCountry0(): \yii\db\ActiveQuery
     {
         return $this->hasOne(Countries::class, ['idCountry' => 'idCountry']);
     }
@@ -100,7 +102,7 @@ class Reservation extends \yii\db\ActiveRecord
      *
      * @return \yii\db\ActiveQuery
      */
-    public function getIdEmployee0()
+    public function getIdEmployee0(): \yii\db\ActiveQuery
     {
         return $this->hasOne(Employees::class, ['idEmployee' => 'idEmployee']);
     }
@@ -110,14 +112,14 @@ class Reservation extends \yii\db\ActiveRecord
      *
      * @return \yii\db\ActiveQuery
      */
-    public function getIdResort0()
+    public function getIdResort0(): \yii\db\ActiveQuery
     {
         return $this->hasOne(Resorts::class, ['idResorts' => 'idResort']);
     }
 
     public static function AddReservation($model)
     {
-        $Employee= Date('d') % 2 === 0 ?
+        $Employee = Date('d') % 2 === 0 ?
             Employees::find()->select('idEmployee')->where(['idEmployee' => 1])->asArray()->all() :
             Employees::find()->select('idEmployee')->where(['idEmployee' => 2])->asArray()->all();
         if (!Yii::$app->user->isGuest)
@@ -125,29 +127,28 @@ class Reservation extends \yii\db\ActiveRecord
             $model->idCity = $_POST["Reservation"]["idCity"];
             $model->idCountry = $_POST["Reservation"]["idCountry"];
             $model->idResort = $_POST["Reservation"]["idResort"];
-            $model->idEmployee =$Employee[0]['idEmployee'];
+            $model->idEmployee = $Employee[0]['idEmployee'];
             $model->LengthOfNights = $_POST["Reservation"]["LengthOfNights"];
             $model->DateBirth = Yii::$app->user->identity->DateBirth;
             $model->mail = Yii::$app->user->identity->Mail;
             $model->Number = Yii::$app->user->identity->Number;
             $model->NumberOfPeople = $_POST["Reservation"]["NumberOfPeople"];
-            $model->save();
-            return $model->save();
+            $model->DateAdded = Date('Y-m-d G:i:s');
         }
         elseif (Yii::$app->user->isGuest)
         {
-
             $model->idCity = $_POST["Reservation"]["idCity"];
             $model->idCountry = $_POST["Reservation"]["idCountry"];
             $model->idResort = $_POST["Reservation"]["idResort"];
-            $model->idEmployee =$Employee[0]['idEmployee'];
+            $model->idEmployee = $Employee[0]['idEmployee'];
             $model->LengthOfNights = $_POST["Reservation"]["LengthOfNights"];
             $model->DateBirth = $_POST["Reservation"]["DateBirth"];
-            $model->mail =$_POST["Reservation"]["mail"];
+            $model->mail = $_POST["Reservation"]["mail"];
             $model->Number = $_POST["Reservation"]["Number"];
             $model->NumberOfPeople = $_POST["Reservation"]["NumberOfPeople"];
-            $model->save();
-            return $model->save();
+            $model->DateAdded = Date('Y-m-d G:i:s');
+
         }
+        return $model->save();
     }
 }
